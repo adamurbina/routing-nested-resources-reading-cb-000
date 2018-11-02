ca4168a2d33292826466595b3c1cd97bf514f270
Adam Urbina <adamurbina@gmail.com>
2018-11-02 22:57:28 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,7 @@
 class AuthorsController < ApplicationController
 
   def posts_index
-    @authors = Author.find(params[:id])
+    @author = Author.find(params[:id])
     @posts = @author.posts
     render template: 'posts/index'
   end
